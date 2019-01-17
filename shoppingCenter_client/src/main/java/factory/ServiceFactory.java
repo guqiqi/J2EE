3f2eafd41359b45ec7a3bd021e3ec70290bc0a38
kiki <798838812@qq.com>
2019-01-17 23:00:18 +0800

    modify factory

--- shoppingCenter_client/src/main/java/factory/ServiceFactory.java
+++ shoppingCenter_client/src/main/java/factory/ServiceFactory.java
@@ -2,15 +2,45 @@ package factory;
 
 import service.OrderService;
 import service.UserService;
-import serviceImpl.OrderServiceImpl;
-import serviceImpl.UserServiceImpl;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Hashtable;
 
 public class ServiceFactory {
+    private Context context;
+
+    public ServiceFactory() {
+        try {
+            Hashtable<String, Object> jndiProps = new Hashtable<String, Object>();
+            jndiProps.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+            jndiProps.put(Context.PROVIDER_URL,"http-remoting://localhost:8080");
+
+            context = new InitialContext(jndiProps);
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
     public OrderService getOrderService(){
-        return new OrderServiceImpl();
+        OrderService orderService = null;
+        try {
+            orderService = (OrderService) context.lookup("ejb:/ShoppingCenter/OrderServiceImpl" +
+                    "!main.java.service.OrderService");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        return orderService;
     }
 
-    public UserService getUserService(){
-        return new UserServiceImpl();
+    public UserService getUserService() {
+        UserService userService = null;
+        try {
+            userService = (UserService) context.lookup("ejb:/ShoppingCenter/UserServiceImpl" +
+                    "!main.java.service.UserService");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        return userService;
     }
 }
